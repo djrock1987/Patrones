5e03a113ae537bccdfab3d900326fbaaa67b13bf
jacastrorug <jacastrorug@PGCAMK727950.local>
2017-04-03 21:40:38 -0500

    Corrección imagen proyecto Patrones
    Se corrige el error java.image.io.IOexception por el archivo smile0.png

--- Patrones/src/paintables/ImageFace.java
+++ Patrones/src/paintables/ImageFace.java
@@ -12,38 +12,39 @@ import javax.imageio.ImageIO;
 
 public class ImageFace extends PaintableBase {
 
-  private BufferedImage bufferedImage;
+	private BufferedImage bufferedImage;
 
-  // --------------------------------------------------------------------------------
+	// --------------------------------------------------------------------------------
 
-  public ImageFace(int x1, int y1, int x2, int y2, int state) {
-    super(x1, y1, x2, y2);
+	public ImageFace(int x1, int y1, int x2, int y2, int state) {
+		super(x1, y1, x2, y2);
 
-   //  Opcion A
-        try {
-          bufferedImage = ImageIO.read(new File("smile0.png"));
-        } catch (IOException e) {
-          throw new RuntimeException(e);
-        }
+		// Opcion A
+		try {
+			bufferedImage = ImageIO.read(new File("smile0.png"));
+			//bufferedImage=ImageCache.getInstance().getImage("smile0.png");
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 
-    // Opcion B
-//    switch (state) {
-//      case SmileConstants.SMILE_DW :
-//        bufferedImage = ImageCache.getInstance().getImage("smile0.png");
-//        break;
-//      case SmileConstants.SMILE_OK :
-//        bufferedImage = ImageCache.getInstance().getImage("smile1.png");
-//        break;
-//      case SmileConstants.SMILE_UP :
-//        bufferedImage = ImageCache.getInstance().getImage("smile2.png");
-//        break;
-//    }
-  }
+		// Opcion B
+		// switch (state) {
+		// case SmileConstants.SMILE_DW :
+		// bufferedImage = ImageCache.getInstance().getImage("smile0.png");
+		// break;
+		// case SmileConstants.SMILE_OK :
+		// bufferedImage = ImageCache.getInstance().getImage("smile1.png");
+		// break;
+		// case SmileConstants.SMILE_UP :
+		// bufferedImage = ImageCache.getInstance().getImage("smile2.png");
+		// break;
+		// }
+	}
 
-  // --------------------------------------------------------------------------------
+	// --------------------------------------------------------------------------------
 
-  @Override
-  public void draw(Graphics2D g2d) {
-    g2d.drawImage(bufferedImage, x1, y1, x2 - x1, y2 - y1, null);
-  }
+	@Override
+	public void draw(Graphics2D g2d) {
+		g2d.drawImage(bufferedImage, x1, y1, x2 - x1, y2 - y1, null);
+	}
 }
