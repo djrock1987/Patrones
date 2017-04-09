b03bcbee5c33a575ded86c831d382f35e904fb8b
Antonio Paternina <acpat88@gmail.com>
2017-04-09 17:41:00 -0500

    ajustes finales en netbeans

--- Patrones2/src/tools/normal/NormalPaintableFactory.java
+++ Patrones2/src/tools/normal/NormalPaintableFactory.java
@@ -2,6 +2,7 @@ package tools.normal;
 
 import common.Paintable;
 import common.PaintableFactory;
+import common.SmileConstants;
 import paintables.DrawnFace;
 
 // Factory
@@ -9,11 +10,11 @@ public class NormalPaintableFactory implements PaintableFactory {
 
     private boolean useImage = false;
 
-  public Paintable create(int x1, int y1, int x2, int y2) {
-       if (useImage) {
-    return new NormalImageFace(x1, y1, x2, y2);
-       } else {
-         return new DrawnFace(x1, y1, x2, y2,4);
-       }
-  }
+    public Paintable create(int x1, int y1, int x2, int y2) {
+        if (useImage) {
+            return new NormalImageFace(x1, y1, x2, y2);
+        } else {
+            return new DrawnFace(x1, y1, x2, y2, SmileConstants.SMILE_OK);
+        }
+    }
 }
